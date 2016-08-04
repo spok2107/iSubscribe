21cbc0d64298664ae2f47491aff5a15154e234b5
spok2107 <ykim2107@hotmail.com>
2016-08-04 13:59:03 -0700

    minor changes

---
+++ iSubscribe/BillRate.swift
@@ -0,0 +1,14 @@
+//
+//  BillRate.swift
+//  iSubscribe
+//
+//  Created by YK on 8/4/16.
+//  Copyright © 2016 Yevgeniy Kim. All rights reserved.
+//
+
+import Foundation
+
+class BillRate {
+    
+    var cost = ""
+}
